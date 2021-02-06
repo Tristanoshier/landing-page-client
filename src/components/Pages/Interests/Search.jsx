736e54917b79eb29a5df8cdb1f3cb1a55ebd94d4
Tristanoshier <tsoshier@bsu.edu>
2021-02-05 19:08:01 -0500

    fix search bar bug

--- src/components/Pages/Interests/Search.jsx
+++ src/components/Pages/Interests/Search.jsx
@@ -3,109 +3,94 @@ import { Input, Form } from 'reactstrap';
 
 export const Search = (props) => {
     const [filteredPosts, setFilteredPosts] = useState([]);
+    const [searchTerm, setSearchTerm] = useState('');
     const [topic, setTopic] = useState('');
-    const [noResults, setNoResults] = useState(false);
+    
+    useEffect(() => {
+        filterSearch();
+    }, [topic, searchTerm])
 
     const handleSubmit = (e) => e.preventDefault();
 
-
-    useEffect(() => {
-        filterSearch();
-    }, [topic])
-
     const filterSearch = () => {
-        let entry = document.getElementById('search').value.toLowerCase();
-        if (entry === '') {
-            setNoResults(false)
 
-            if (topic === 'music') {
-                setFilteredPosts(props.musicPosts);
-            } else if (topic === 'movies') {
-                setFilteredPosts(props.moviePosts);
-            } else if (topic === 'programming') {
-                setFilteredPosts(props.programmingPosts);
-            } else {
-                setFilteredPosts([]);
-            }
-
-        } else {
-            let filtered;
-            if (topic === 'music') {
-                filtered = props.musicPosts.filter(post => {
-                    if (post.title.toLowerCase().includes(entry.trim())) {
-                        return post;
-                    } else {
-                        return ''
-                    }
-                })
-            } else if (topic === 'movies') {
-                filtered = props.moviePosts.filter(post => {
-                    if (post.title.toLowerCase().includes(entry.trim())) {
-                        return post
-                    } else {
-                        return ''
-                    }
-                })
-
-            } else if (topic === 'programming') {
-                filtered = props.programmingPosts.filter(post => {
-                    if (post.title.toLowerCase().includes(entry.trim())) {
-                        return post
-                    } else {
-                        return ''
-                    }
-                })
-
-            } else {
-                filtered = props.posts.filter(post => {
-                    if (post.title.toLowerCase().includes(entry.trim())) {
-                        return post
-                    } else {
-                        return ''
-                    }
-                })
-            }
-            setFilteredPosts(filtered);
+        if (topic === 'music') {
+            setFilteredPosts(props.musicPosts);
+        } else if (topic === 'movies') {
+            setFilteredPosts(props.moviePosts);
+        } else if (topic === 'programming') {
+            setFilteredPosts(props.programmingPosts);
+        } else if (topic === '') {
+            setFilteredPosts(props.posts);
         }
+
+        let filtered;
+        if (topic === 'music') {
+            filtered = props.musicPosts.filter(post => {
+                if (searchTerm === '') {
+                    return props.musicPosts;
+                } else if (post.title.toLowerCase().includes(searchTerm.toLowerCase().trim())) {
+                    return post;
+                } return null;
+            })
+        } else if (topic === 'movies') {
+            filtered = props.moviePosts.filter(post => {
+                if (searchTerm === '') {
+                    return props.moviePosts;
+                } else if (post.title.toLowerCase().includes(searchTerm.toLowerCase().trim())) {
+                    return post;
+                } return null;
+            })
+        } else if (topic === 'programming') {
+            filtered = props.programmingPosts.filter(post => {
+                if (searchTerm === '') {
+                    return props.programmingPosts;
+                } else if (post.title.toLowerCase().includes(searchTerm.toLowerCase().trim())) {
+                    return post;
+                } return null;
+            })
+        } else if (topic === '') {
+            filtered = props.posts.filter(post => {
+                if (searchTerm === '') {
+                    return props.posts;
+                } else if (post.title.toLowerCase().includes(searchTerm.toLowerCase().trim())) {
+                    return post;
+                } return null;
+            })
+        }
+        setFilteredPosts(filtered);
     }
 
 
     const postMapper = () => {
-        let posts;
-        filteredPosts.length === 0 ? posts = props.posts :
-            posts = filteredPosts;
-        if (noResults === true) {
+        if (filteredPosts.length > 0) {
+            return filteredPosts.map((post, index) => {
+                return (
+                    <div key={index}>
+                        <h2 className={props.darkMode ? "sub-heading-dark" : "sub-heading-light"}>- {post.title} -</h2>
+                        <h5>{post.createdAt}</h5>
+                        <p className={props.darkMode ? "about-text-dark" : "about-text-light"}>
+                            {post.body}
+                        </p>
+                    </div>
+                )
+            })
+        } else {
             return (
                 <h2 className={props.darkMode ? "about-text-dark" : "about-text-light"}>
                     no results found
                 </h2>
             )
-        } else {
-            if (posts.length > 0) {
-                return posts.map((post, index) => {
-                    return (
-                        <div key={index}>
-                            <h2 className={props.darkMode ? "sub-heading-dark" : "sub-heading-light"}>- {post.title} -</h2>
-                            <h5>{post.createdAt}</h5>
-                            <p className={props.darkMode ? "about-text-dark" : "about-text-light"}>
-                                {post.body}
-                            </p>
-                        </div>
-                    )
-                })
-            } else {
-                return (
-                    <h2 className={props.darkMode ? "about-text-dark" : "about-text-light"}>
-                        Nothing is currently posted
-                    </h2>
-                )
-            }
+
         }
     }
 
     const chooseTopic = (e) => {
         setTopic(e.target.value);
-        filterSearch();
+    }
+
+    const userSearch = (e) => {
+        setSearchTerm(e.target.value);
     }
 
     return (
@@ -117,7 +102,7 @@ export const Search = (props) => {
                     <option value="music">Music</option>
                     <option value="movies">Movies</option>
                 </Input>
-                <input onChange={filterSearch} id="search" type="text" placeholder="Search by title" />
+                <input onChange={(e) => userSearch(e)} id="search" type="text" placeholder="Search by title" />
             </Form>
             {postMapper()}
         </div>
