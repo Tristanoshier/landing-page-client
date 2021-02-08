a43311013a7c988a357a33bc2ef0d2533542d93d
Tristanoshier <tsoshier@bsu.edu>
2021-02-08 18:57:52 -0500

    refactor blog component

--- src/components/Pages/Blog.jsx
+++ src/components/Pages/Blog.jsx
@@ -3,12 +3,12 @@ import { withRouter } from 'react-router-dom';
 import backToTop from '../Shared/BackToTop';
 import postMapper from '../Shared/PostMapper';
 
-
 const Blog = (props) => {
     const [blogPosts, setBlogPosts] = useState([]);
+    const blogURL = `http://localhost:3001/site/blog`;
 
     useEffect(() => {
-        fetch(`http://localhost:3001/site/blog`, {
+        fetch(blogURL, {
             method: 'GET',
             headers: new Headers({
                 'Content-Type': 'application/json'
@@ -17,20 +17,20 @@ const Blog = (props) => {
             .then((posts) => {
                 setBlogPosts(posts);
             })
-    }, [])
+    }, [blogURL])
 
     return (
         <>
-          <div className="blog">
-            <p id="main-header">Blog</p>
-            <p className={props.darkMode ? "about-text-dark" : "about-text-light"}>
-                Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in
-            </p>
-            {postMapper(blogPosts, 'blog', props.darkMode)}
-        </div>
-        <button className={props.darkMode ? "back-to-top-btn" : "back-to-top-btn light"} onClick={() => backToTop()}>Back to top</button>
+            <div className='blog'>
+                <p id='main-header'>Blog</p>
+                <p className={props.darkMode ? 'about-text-dark' : 'about-text-light'}>
+                    Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in
+                </p>
+                {postMapper(blogPosts, 'blog', props.darkMode)}
+            </div>
+            <button className={props.darkMode ? 'back-to-top-btn' : 'back-to-top-btn light'} onClick={() => backToTop()}>Back to top</button>
         </>
-      
+
     )
 }
 
