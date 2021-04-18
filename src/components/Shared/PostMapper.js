87b468a7e13c60d34ccafb570df51e6bcdeba8e0
Tristanoshier <tsoshier@bsu.edu>
2021-04-18 17:24:18 -0400

    add text formatting to post preview

--- src/components/Shared/PostMapper.js
+++ src/components/Shared/PostMapper.js
@@ -38,8 +38,8 @@ const postMapper = (posts, type, lastElementOnPage, darkMode) => {
                             }
                             {
                                 post.body.length > 300 ?
-                                    <p className='card-body'>{post.body.substr(1, 300)}. . . <Link onClick={() => savePostInLocalStorage(post)} className={darkMode ? 'card-link' : 'card-link light'} to={{ pathname: `/Focused/${post}`, post: post }}>Read More</Link></p>
-                                    : <p className='card-body'>{post.body}</p>
+                                    <p className='card-body'><div dangerouslySetInnerHTML={{__html: `${post.body.substr(1, 300)}. . .`}} /> <Link onClick={() => savePostInLocalStorage(post)} className={darkMode ? 'card-link' : 'card-link light'} to={{ pathname: `/Focused/${post}`, post: post }}>Read More</Link></p>
+                                    : <p className='card-body'><div dangerouslySetInnerHTML={{__html: post.body}} /></p>
                             }
                         </div>
                     )
