39f64bd415788abe47b1fc764cb44372eb88a50a
Tristanoshier <tsoshier@bsu.edu>
2022-10-04 15:26:38 -0400

    build services page

--- src/components/Pages/Services/Services.jsx
+++ src/components/Pages/Services/Services.jsx
@@ -6,54 +6,63 @@ const Services = () => {
     <div id="services" className="services">
       <div className="page-header">Services I Offer</div>
       <div className="services-container">
-        <div className="service-option">
-          <div className="service-title">01 Personal Portfolio</div>
-          <div className="service-description">
-            There are many variations of passages of Lorem Ipsum available, but
-            the majority have suffered alteration in some form, by injected
-            humour, or randomised words which don't look even slightly
-            believable. If you are going to use a passage of Lorem Ipsum, you
-            need to be sure there isn't anything embarrassing hidden in the
-            middle of text. All the Lorem Ipsum generators on the Internet tend
-            to repeat predefined chunks as necessary, making this the first true
-            generator on the Internet.
-          </div>
-          <Link to="/portfolio-service">
-            <button>See Details</button>
-          </Link>
-        </div>
-        <div className="service-option">
-          <div className="service-title">02 Online Store</div>
-          <div className="service-description">
-            There are many variations of passages of Lorem Ipsum available, but
-            the majority have suffered alteration in some form, by injected
-            humour, or randomised words which don't look even slightly
-            believable. If you are going to use a passage of Lorem Ipsum, you
-            need to be sure there isn't anything embarrassing hidden in the
-            middle of text. All the Lorem Ipsum generators on the Internet tend
-            to repeat predefined chunks as necessary, making this the first true
-            generator on the Internet.
-          </div>
-          <Link to="/store-service">
-            <button>See Details</button>
-          </Link>
-        </div>
-        <div className="service-option">
-          <div className="service-title">03 Consultation</div>
-          <div className="service-description">
-            There are many variations of passages of Lorem Ipsum available, but
-            the majority have suffered alteration in some form, by injected
-            humour, or randomised words which don't look even slightly
-            believable. If you are going to use a passage of Lorem Ipsum, you
-            need to be sure there isn't anything embarrassing hidden in the
-            middle of text. All the Lorem Ipsum generators on the Internet tend
-            to repeat predefined chunks as necessary, making this the first true
-            generator on the Internet.
-          </div>
-          <Link to="/consultation-service">
-            <button>See Details</button>
-          </Link>
-        </div>
+        <ul>
+          <li>
+            <input type="checkbox" defaultChecked />
+            <i></i>
+            <h2><strong>01</strong> Personal Portfolio</h2>
+            <p>
+              This page was written in HTML and CSS. The CSS was compiled from
+              SASS. I used Normalize as my CSS reset and -prefix-free to save
+              myself some headaches. I haven't quite gotten the hang of Slim for
+              compiling into HTML, but someday I'll use it since its syntax
+              compliments that of SASS. Regardless, this could all be done in
+              plain HTML and CSS.
+              <br />
+              <div className="btn-container">
+                <Link className="btn-link" to="/portfolio-service">
+                  <button className="landing-btn browse-all">See Details</button>
+                </Link>
+              </div>
+            </p>
+          </li>
+          <li>
+            <input type="checkbox" defaultChecked />
+            <i></i>
+            <h2><strong>02</strong> Online Store</h2>
+            <p>
+              Using the sibling and checked selectors, we can determine the
+              styling of sibling elements based on the checked state of the
+              checkbox input element. One use, as demonstrated here, is an
+              entirely CSS and HTML accordion element. Media queries are used to
+              make the element responsive to different screen sizes.
+              <br />
+              <div className="btn-container">
+                <Link className="btn-link" to="/store-service">
+                  <button className="landing-btn browse-all">See Details</button>
+                </Link>
+              </div>
+            </p>
+          </li>
+          <li>
+            <input type="checkbox" defaultChecked />
+            <i></i>
+            <h2><strong>03</strong> Consultation</h2>
+            <p>
+              By making the open state default for when :checked isn't detected,
+              we can make this system accessable for browsers that don't
+              recognize :checked. The fallback is simply an open accordion. The
+              accordion can be manipulated with Javascript (if needed) by
+              changing the "checked" property of the input element.
+              <br />
+              <div className="btn-container">
+                <Link className="btn-link" to="/consultation-service">
+                  <button className="landing-btn browse-all">See Details</button>
+                </Link>
+              </div>
+            </p>
+          </li>
+        </ul>
       </div>
     </div>
   );
