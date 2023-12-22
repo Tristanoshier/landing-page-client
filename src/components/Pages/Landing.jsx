d99133c372d0770132b6c3b23e83251b4527fb5b
Tristanoshier <tsoshier@bsu.edu>
2023-12-22 17:33:31 -0500

    styling for mobile

--- src/components/Pages/Landing.jsx
+++ src/components/Pages/Landing.jsx
@@ -1,6 +1,13 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Spring } from "react-spring/renderprops";
 const Landing = () => {
+  const [isMobile, setIsMobile] = useState(false);
+
+  useEffect(() => {
+    if (window.innerWidth <= 760) {
+      setIsMobile(true);
+    }
+  }, []);
   return (
     <section className="landing-section">
       <Spring
@@ -10,18 +17,46 @@ const Landing = () => {
       >
         {(props) => (
           <div style={props}>
-            <div className="landing-container">
-            <video className="landing-video" autoplay="autoplay" loop="loop" muted defaultMuted playsinline  oncontextmenu="return false;"  preload="auto"  id="myVideo">
-              <source src="/video/landing_vid.mp4" type="video/mp4" />
-            </video>
-            </div>
-            <div className="landing-container-mobile">
-              <img className="landing-img" src="/images/inMyRoom-1.jpg" alt="" />
-              <img className="landing-img" src="/images/inMyRoom-2.jpg" alt="" />
-              <img className="landing-img" src="/images/inMyRoom-3.jpg" alt="" />
-              <img className="landing-img" src="/images/inMyRoom-4.jpg" alt="" />
-            </div>
-        
+            {!isMobile ? (
+              <div className="landing-container">
+                <video
+                  className="landing-video"
+                  autoplay="autoplay"
+                  loop="loop"
+                  muted
+                  defaultMuted
+                  playsinline
+                  oncontextmenu="return false;"
+                  preload="auto"
+                  id="myVideo"
+                >
+                  <source src="/video/landing_vid.mp4" type="video/mp4" />
+                </video>
+              </div>
+            ) : (
+              <></>
+            )}
+            {isMobile ? (
+              <div className="landing-container-mobile">
+                <img
+                  className="landing-img"
+                  src="/images/inMyRoom-1.jpg"
+                  alt=""
+                />
+                <img
+                  className="landing-img"
+                  src="/images/inMyRoom-2.jpg"
+                  alt=""
+                />
+                <img
+                  className="landing-img"
+                  src="/images/inMyRoom-4.jpg"
+                  alt=""
+                />
+              </div>
+            ) : (
+              <></>
+            )}
           </div>
         )}
       </Spring>
