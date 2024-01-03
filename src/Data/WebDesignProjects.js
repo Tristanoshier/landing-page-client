b05a985e412e594720b2fa00a4be28e39c1ac05e
Tristanoshier <tsoshier@bsu.edu>
2024-01-02 19:09:49 -0500

    remove + add photos

--- src/Data/WebDesignProjects.js
+++ src/Data/WebDesignProjects.js
@@ -4,28 +4,28 @@ var webDesignProjects = [
     title: "Bernard James",
     description: "Fine Jewelry handcrafted in NYC",
     link: "https://bernardjames.com",
-    thumbnailpath: "/images/bernardjames.png",
+    thumbnailpath: "/images/webDesignPhoto-3.jpg",
   },
   {
     id: 2,
     title: "Alice Montana",
     description: "Hair stylist, makeup artist, and esthetician",
     link: "https://alicemontana.com",
-    thumbnailpath: "/images/alicemontana.png",
+    thumbnailpath: "/images/webDesignPhoto-1.jpg",
   },
   {
     id: 3,
     title: "Frogman Leather",
     description: "Handcrafted leather goods in Loveland, Ohio",
     link: "https://www.frogmanleather.com/",
-    thumbnailpath: "/images/frogmanleather.png",
+    thumbnailpath: "/images/webDesignPhoto-4.jpg",
   },
   {
     id: 4,
     title: "Isabelle Miller",
     description: "Sync licensing and brand partnerships portoflio",
     link: "https://isabelle-miller.web.app",
-    thumbnailpath: "/images/isabellemiller.png",
+    thumbnailpath: "/images/webDesignPhoto-2.jpg",
   },
 ];
 
