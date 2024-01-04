d78cc9bba91be74f2bdf0c64940b43fa8f6bd2f5
Tristanoshier <tsoshier@bsu.edu>
2024-01-03 20:33:35 -0500

    change titles of photo projects

--- src/Data/photoProjects.js
+++ src/Data/photoProjects.js
@@ -1,12 +1,12 @@
 var photoProjects = [
   {
     id: 1,
-    title: "Grocery shopping in Queens with Eli for Asmara",
+    title: "Eli for Asmara",
     thumbnailImage: "/images/asmaranyc-01.jpg",
   },
   {
     id: 2,
-    title: "Building furniture with Dan for Carmen & Co.",
+    title: "Dan for CARMEN & CO.",
     thumbnailImage: "/images/carmenAndCoDan-08.jpg",
   },
 ];
